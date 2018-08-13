0a7e1cc97a9e6ae2b1abf8710e4f74b43663728e
blakemacnair <blakemacnair@gmail.com>
2018-08-13 18:28:35 -0500

    Add extension to Acronym to conform to Model.

--- Sources/App/Models/Acronym.swift
+++ Sources/App/Models/Acronym.swift
@@ -1,6 +1,7 @@
 import Vapor
 import FluentSQLite
 
+// All Fluent models must conform to Codable
 final class Acronym: Codable {
     var id: Int?
     var short: String
@@ -10,4 +11,16 @@ final class Acronym: Codable {
         self.short = short
         self.long = long
     }
+}
+
+// Conform to Fluent's Model
+extension Acronym: Model {
+    // The type of DB to use for this model
+    typealias Database = SQLiteDatabase
+
+    // The type of this model's ID
+    typealias ID = Int
+
+    // The key path of the model's ID property
+    static var idKey: IDKey = \Acronym.id
 }

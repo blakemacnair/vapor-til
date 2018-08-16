217d95ababb69629cf5355a87772b6271b425c79
blakemacnair <blakemacnair@gmail.com>
2018-08-15 21:34:37 -0500

    Fix a missed enum case in configure.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -33,7 +33,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
     /// Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(model: Acronym.self, database: .sqlite)
+    migrations.add(model: Acronym.self, database: .mysql)
     services.register(migrations)
 
 }

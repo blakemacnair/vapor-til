9eeba1e768502e89559a9f13ca4a16c19492c2ab
blakemacnair <blakemacnair@gmail.com>
2018-08-16 15:27:50 -0500

    Add GET request to return the first Acronym.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -38,6 +38,20 @@ public func routes(_ router: Router) throws {
             }.all()
     }
 
+    // GET the first result from a search parameter
+    router.get("api", "acronyms", "first") { req -> Future<Acronym> in
+        // TODO: Make this searchable
+        return Acronym.query(on: req)
+            .first()
+            .map(to: Acronym.self, { acronym -> Acronym in
+                guard let acronym = acronym else {
+                    throw Abort(.notFound)
+                }
+
+                return acronym
+        })
+    }
+
     // PUT update a single acronym
     router.put("api", "acronyms", Acronym.parameter) { req -> Future<Acronym> in
         return try flatMap(to: Acronym.self,

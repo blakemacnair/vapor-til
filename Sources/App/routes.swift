730cf1b3f6525152c0423a69ac2cd55bb1143ddf
blakemacnair <blakemacnair@gmail.com>
2018-08-16 22:18:51 -0500

    Remove hello world route.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,11 +3,7 @@ import Fluent
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
-
+    // Register Acronyms Controller and its routes
     let acronymsController = AcronymsController()
     try router.register(collection: acronymsController)
 }

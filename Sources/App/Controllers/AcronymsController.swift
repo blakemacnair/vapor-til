297436cf056ac8d4271d8ff34630f1e5f101bdb8
blakemacnair <blakemacnair@gmail.com>
2018-08-16 22:00:31 -0500

    Add a shorthand var for the acronyms API route.

--- Sources/App/Controllers/AcronymsController.swift
+++ Sources/App/Controllers/AcronymsController.swift
@@ -2,10 +2,14 @@ import Vapor
 import Fluent
 
 struct AcronymsController: RouteCollection {
+
     func boot(router: Router) throws {
-        router.get("api", "acronyms", use: getAllHandler)
+        let acronymsRoutes = router.grouped("api", "acronyms")
+
+        acronymsRoutes.get(use: getAllHandler)
     }
 
+    // GET a list of all acronyms
     func getAllHandler(_ req: Request) throws -> Future<[Acronym]> {
         return Acronym.query(on: req).all()
     }
